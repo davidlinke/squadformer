fcb3dfcced81b5791142f2c36884ad65edaadbd6
davidlinke <davidlinke@davidlinke.com>
2019-08-02 14:02:36 -0400

    can now create a list of names in the database

--- controllers/squad_controller.js
+++ controllers/squad_controller.js
@@ -3,4 +3,28 @@ const squad = express.Router();
 
 const group = require('../models/group.js');
 
+squad.get('/new', (req, res) => {
+	res.render('newSquad.ejs');
+});
+
+squad.post('/', (req, res) => {
+	// console.log(req.body);
+	const names = {
+		names: separateNames(req.body.names)
+	};
+	// console.log(names);
+	group.create(names, (err, createdSquad) => {
+		if (err) {
+			console.log(err);
+		}
+		// console.log(createdSquad);
+		res.redirect('/');
+	});
+});
+
 module.exports = squad;
+
+// Split names by newline into array of strings
+const separateNames = stringOfNames => {
+	return stringOfNames.split('\r\n');
+};
