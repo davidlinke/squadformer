50ad48c0aac4ee8db9b04a526f9dc94b9c6a309e
davidlinke <davidlinke@davidlinke.com>
2019-08-11 12:10:51 -0400

    working on making time formatting more robust

--- public/js/savedGroup.js
+++ public/js/savedGroup.js
@@ -1,4 +1,9 @@
 let time = $('#time').text();
+console.log(time);
 time = moment(time).format('MMMM Do YYYY, h:mm a');
+// time = moment(time, 'YYY-MM-DDTHH:MM:SS.928Z').format('MMMM Do YYYY, h:mm a');
 
 $('.boldTime').text(time);
+
+// Sun Aug 11 2019 11:21:59 GMT-0400 (Eastern Daylight Time)
+// ddd MMM DD YYYY
