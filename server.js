41c8e3b80895010573da511431152d2276672841
davidlinke <davidlinke@davidlinke.com>
2019-08-11 13:22:53 -0400

    added 404/500 error handling and error page

--- server.js
+++ server.js
@@ -12,10 +12,26 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
+
 app.use('/squads', squadController);
 
 app.get('/', (req, res) => {
 	res.render('index.ejs');
+});
+
+app.use(function(req, res) {
+	res.status(400);
+	console.log('there seems to be a 404 error');
+	res.render('error.ejs', { title: '404: File Not Found' });
+});
+
+app.use(function(error, req, res, next) {
+	res.status(500);
+	console.log('there seems to be a 500 error');
+	res.render('error.ejs', {
+		title: '500: Internal Server Error',
+		error: error
+	});
 });
 
 app.listen(PORT, () => console.log('Listening on port:', PORT));
