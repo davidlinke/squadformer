78c1c2a5e960e4a60f582d48216fcf5e0ce783cf
davidlinke <davidlinke@davidlinke.com>
2019-08-11 12:27:29 -0400

    now parsing time correctly for moment.js, no more deprecation warnings shown

--- public/js/savedGroup.js
+++ public/js/savedGroup.js
@@ -1,9 +1,4 @@
 let time = $('#time').text();
-console.log(time);
-time = moment(time).format('MMMM Do YYYY, h:mm a');
-// time = moment(time, 'YYY-MM-DDTHH:MM:SS.928Z').format('MMMM Do YYYY, h:mm a');
+time = moment(time, 'ddd MMM DD YYYY HH:mm:ss Z').format('MMMM Do YYYY, h:mma');
 
 $('.boldTime').text(time);
-
-// Sun Aug 11 2019 11:21:59 GMT-0400 (Eastern Daylight Time)
-// ddd MMM DD YYYY
